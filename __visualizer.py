a41a9bb44657a01642a268065509414f916af4e2
K1o0n <tima2licey@gmail.com>
2025-03-16 11:19:52 +0300

    added saving image

--- __visualizer.py
+++ __visualizer.py
@@ -1,6 +1,6 @@
 import numpy as np
 from PIL import Image
-def visualize(arr: list[list[int]]) -> str:
+def visualize(arr: list[list[int]], name: str) -> str:
     """Функция, которая возвращает картинку.
 
     Args:
@@ -15,7 +15,8 @@ def visualize(arr: list[list[int]]) -> str:
         for j in range(64):
             data[i,j] = [color[0] * (arr[i][j]/255), color[1] * (arr[i][j]/255), color[2] * (arr[i][j]/255)]
     image = Image.fromarray(data)      # Create a PIL image
+    image.save(f"images/{name}.png")
     image.show()
 if __name__ == "__main__":
     YOUR_MOTHER = []
-    visualize(YOUR_MOTHER)
+    visualize(YOUR_MOTHER, "your_mother")
