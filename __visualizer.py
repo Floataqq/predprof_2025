3a01e3b06c99701cf760a2bfadedf600a8b8d669
K1o0n <tima2licey@gmail.com>
2025-03-16 11:15:49 +0300

    goida

---
+++ __visualizer.py
@@ -0,0 +1,21 @@
+import numpy as np
+from PIL import Image
+def visualize(arr: list[list[int]]) -> str:
+    """Функция, которая возвращает картинку.
+
+    Args:
+        arr (list[list[int]]): Массив данных из api
+
+    Returns:
+        str: path до картинки
+    """
+    color = (219,73,44)
+    data = np.zeros((64,64,3), dtype=np.uint8)
+    for i in range(64):
+        for j in range(64):
+            data[i,j] = [color[0] * (arr[i][j]/255), color[1] * (arr[i][j]/255), color[2] * (arr[i][j]/255)]
+    image = Image.fromarray(data)      # Create a PIL image
+    image.show()
+if __name__ == "__main__":
+    YOUR_MOTHER = []
+    visualize(YOUR_MOTHER)
